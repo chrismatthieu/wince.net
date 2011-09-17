f9fd1760243239b397dff5c08d107f640ea5cebc
Chris Matthieu <chris@matthieu.us>
2011-09-17 07:30:36 -0700

    updated text

--- wince.rb
+++ wince.rb
@@ -2,7 +2,7 @@ require "rubygems"
 require "sinatra"
 
 get "/" do
-  html = '<html><body><br/><br/><center><h1>WinCE.net<br/>Coming Soon...</h1></center></body></html>'
+  html = '<html><body><br/><br/><center><h1>WinCE.net<br/>This domain is available for sale or lease<br/>Contact chris [at] getvocal.com</h1></center></body></html>'
   return html
 end
 
